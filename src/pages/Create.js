20ba7e6b18b3503fb66b3563fdf6edde15c87bf3
juliadavydenko <julietta12345@yahoo.com>
2023-08-10 14:41:30 +0200

    added components, typography, buttons, buttongroup, container, imported icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import { Typography, Button, Container } from "@mui/material";
+import AcUnitOutlinedIcon from "@mui/icons-material/AcUnitOutlined";
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a new Note
+      </Typography>
+      <Button
+        onClick={() => console.log("you clicked")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+      <AcUnitOutlinedIcon />
+    </Container>
+  );
 }
