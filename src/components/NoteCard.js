ef776d4d9e737bfac67746317a687efa769b4f4d
juliadavydenko <julietta12345@yahoo.com>
2023-08-12 17:48:52 +0200

    fixed the issue, so now avatars are colored in corresponding colors depending on their category

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -4,22 +4,47 @@ import { CardHeader } from "@mui/material";
 import { CardContent } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { DeleteOutlineOutlined } from "@mui/icons-material";
+import { blue, green, pink, yellow } from "@mui/material/colors";
 const useStyles = makeStyles({
-  test: {
-    border: (note) => {
+  avatar: {
+    backgroundColor: (note) => {
       if (note.category == "work") {
-        return "1px solid red";
+        return yellow[700];
       }
+      if (note.category == "money") {
+        return green[500];
+      }
+      if (note.category == "todos") {
+        return pink[500];
+      }
+      return blue[500];
     },
   },
 });
+const AvatarColor = (note) => {
+  if (note.category === "work") {
+    return yellow[700];
+  }
+  if (note.category === "money") {
+    return green[500];
+  }
+  if (note.category === "todos") {
+    return pink[500];
+  }
+  return blue[500];
+};
+
 function NoteCard({ note, handleDelete }) {
   const classes = useStyles(note);
   return (
     <div>
-      <Card elevation={1} className={classes.test}>
+      <Card elevation={1}>
         <CardHeader
-          avatar={<Avatar>{note.category[0].toUpperCase()}</Avatar>}
+          avatar={
+            <Avatar sx={{ backgroundColor: AvatarColor(note) }}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
               <DeleteOutlineOutlined />
