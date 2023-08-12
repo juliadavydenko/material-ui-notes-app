69de1a1ff877e260b545156db9d5c96bb498f5c5
juliadavydenko <julietta12345@yahoo.com>
2023-08-12 16:47:56 +0200

    madded border, red, to outline notes related to work category

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,12 +2,22 @@ import React from "react";
 import { Card, IconButton, Typography } from "@mui/material";
 import { CardHeader } from "@mui/material";
 import { CardContent } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import { DeleteOutlineOutlined } from "@mui/icons-material";
-
+const useStyles = makeStyles({
+  test: {
+    border: (note) => {
+      if (note.category == "work") {
+        return "1px solid red";
+      }
+    },
+  },
+});
 function NoteCard({ note, handleDelete }) {
+  const classes = useStyles(note);
   return (
     <div>
-      <Card elevation={1}>
+      <Card elevation={1} className={classes.test}>
         <CardHeader
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
