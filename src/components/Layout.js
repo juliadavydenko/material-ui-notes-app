7b2c851862845fa8b4aebfaefd689e466d9d58d1
juliadavydenko <julietta12345@yahoo.com>
2023-08-12 16:28:45 +0200

    added responsive navigation between pages, with useHistory

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Typography, Drawer } from "@mui/material";
 import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 import { AddCircleOutlineOutlined, SubjectOutlined } from "@mui/icons-material";
+import { useHistory } from "react-router-dom";
 
 const drawerWidth = 240;
 
@@ -24,6 +25,7 @@ const useStyles = makeStyles({
 
 export default function Layout({ children }) {
   const classes = useStyles();
+  const history = useHistory();
   const menuItems = [
     {
       text: "My Notes",
@@ -55,7 +57,11 @@ export default function Layout({ children }) {
 
         <List>
           {menuItems.map((item) => (
-            <ListItem key={item.text}>
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => history.push(item.path)}
+            >
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.text} />
             </ListItem>
